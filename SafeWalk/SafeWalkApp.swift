236a1ddc6007307583152616b772491d51198405
riyadsarsour <riyadsarsour@live.com>
2023-06-26 20:00:37 -0400

    Initial Commit

---
+++ SafeWalk/SafeWalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeWalkApp.swift
+//  SafeWalk
+//
+//  Created by Riyad Sarsour on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct SafeWalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

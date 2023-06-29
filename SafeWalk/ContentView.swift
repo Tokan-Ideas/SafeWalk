9ed440668d7106960fcb0aae641582cfd5d336d0
rahqis <rahqi@ad.unc.edu>
2023-06-28 21:59:23 -0400

    Report View, added location to mapview, added buttons to mapview

--- SafeWalk/ContentView.swift
+++ SafeWalk/ContentView.swift
@@ -83,8 +83,8 @@ struct MapView: View {
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.3317, longitude: -122.0307), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
 
     var body: some View {
-        Map(coordinateRegion: $region, showsUserLocation: true, userTrackingMode: $tracking) // Add showsUserLocation parameter
-                .ignoresSafeArea(edges: .all)
+        Map(coordinateRegion: $region, interactionModes: .all, showsUserLocation: true, userTrackingMode: $tracking) // Add showsUserLocation parameter
+                .ignoresSafeArea(.all)
                 .onAppear {
                     setRegion(location: LocationManager.shared.lastKnownLocation)
 //
@@ -94,6 +94,59 @@ struct MapView: View {
                     guard let location = location else { return }
                     setRegion(location: location) // Update the region when the location changes
                 }
+                .overlay(alignment: .topLeading) {
+                    HStack(spacing: 20) {
+                        Button {
+                            print("Recent Alerts")
+                            
+                        } label: {
+                            Image(systemName: "bell.fill")
+                        }
+                        .buttonStyle(.borderedProminent)
+                        .buttonBorderShape(.capsule)
+                        Spacer()
+                        Button("Report") {
+                            print("Report Pressed")
+                        }
+                        //                    } label: {
+                        //                        
+                        //                    }
+                        .buttonStyle(.borderedProminent)
+                        .buttonBorderShape(.capsule)
+                    }
+                    .padding()
+                }
+                .overlay(alignment: .bottomTrailing) {
+                    VStack(spacing: 20) {
+//                        if #available(iOS 17, *) {
+//                            MapCompass()
+//                            MapUserLocationButton()
+//                        } else {
+//                            Button {
+//                                print("Left Pressed")
+//                            } label: {
+//                                Image(systemName: "bell.fill")
+//                            }
+//                        }
+                        Button {
+                            print("Left Pressed")
+                            locationManager.requestLocation()
+                            setRegion(location: locationManager.lastKnownLocation)
+                        } label: {
+                            Image(systemName: "location.fill")
+                        }
+                        //.buttonStyle(.borderedProminent)
+                        .buttonBorderShape(.capsule)
+                    }
+                    
+                    .padding()
+                }
+                
+//                .mapControlVisibility(.visible)
+        
+                Spacer()
+                
+                
         
     }
 

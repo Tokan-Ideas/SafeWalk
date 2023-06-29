5eaf9a8f2eab5383304ff366772e9f8a7c524ceb
rahqis <rahqi@ad.unc.edu>
2023-06-29 16:31:27 -0400

    Dummy view for ReportView, debugged live locaiton

--- SafeWalk/ReportView.swift
+++ SafeWalk/ReportView.swift
@@ -8,11 +8,36 @@
 import SwiftUI
 
 struct ReportView: View {
+    @Environment(\.dismiss) var dismiss
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .center, content: {
+            Spacer()
+            HStack(content: {
+                Button("Police") {
+                    print("Police")
+                }
+                Spacer()
+                Button("No Lights") {
+                    print("No Lights")
+                }
+                Spacer()
+                Button("Construction") {
+                    print("Construction")
+                }
+            })
+            Spacer()
+            HStack(alignment: .bottom, content: {
+                Button("Close") {
+                    dismiss()
+                }
+            })
+            .padding()
+            
+        })
+        .padding()
     }
 }
 
-#Preview {
-    ReportView()
-}
+//#Preview {
+//    ReportView()
+//}
